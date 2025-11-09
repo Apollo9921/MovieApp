9979c33478f4a5eec11a505f1d9dd0e03a6da38c
DESKTOP-TBTK50T\Nuno Silva <nunosilvaw33d@gmail.com>
2025-11-09 16:48:33 +0000

    Refactor: Execute network calls on IO dispatcher
    This commit refactors the `MovieRepositoryImpl` to ensure that all network calls made through `movieService` are executed on the `IO` dispatcher.
    
    This is achieved by wrapping each service method call within a `withContext(Dispatchers.IO)` block, which helps in preventing network operations from blocking the main thread.

--- MovieApp-master/app/src/main/java/com/example/movieapp/koin/MovieRepositoryImpl.kt
+++ MovieApp-master/app/src/main/java/com/example/movieapp/koin/MovieRepositoryImpl.kt
@@ -4,24 +4,34 @@ import com.example.movieapp.networking.model.details.MovieDetails
 import com.example.movieapp.networking.model.genres.GenresList
 import com.example.movieapp.networking.model.movies.Movies
 import com.example.movieapp.networking.requests.MovieService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Response
 
 class MovieRepositoryImpl(
     private val movieService: MovieService
-): MoviesRepository {
+) : MoviesRepository {
     override suspend fun fetchMovies(pageNumber: Int): Response<Movies> {
-        return movieService.getMovies(pageNumber)
+        return withContext(Dispatchers.IO) {
+            movieService.getMovies(pageNumber)
+        }
     }
 
     override suspend fun fetchGenres(): Response<GenresList> {
-        return movieService.getGenres()
+        return withContext(Dispatchers.IO) {
+            movieService.getGenres()
+        }
     }
 
     override suspend fun searchMovie(query: String): Response<Movies> {
-        return movieService.searchMovie(query)
+        return withContext(Dispatchers.IO) {
+            movieService.searchMovie(query)
+        }
     }
 
     override suspend fun getMovieDetails(movieId: Int): Response<MovieDetails> {
-        return movieService.getMovieDetails(movieId)
+        return withContext(Dispatchers.IO) {
+            movieService.getMovieDetails(movieId)
+        }
     }
 }
